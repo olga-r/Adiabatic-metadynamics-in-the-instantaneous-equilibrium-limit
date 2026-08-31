2fd73eb18c78673e8fabe66ef6aeef0ab4d709cb
Olga Rogacheva <acerlaetum@yandex.ru>
2026-08-31 06:01:42 +0200

    Add files via upload

---
+++ src/metad/config_manager.py
@@ -0,0 +1,50 @@
+import json
+from pathlib import Path
+import os
+from typing import Any, Dict
+from types import SimpleNamespace
+
+current_dir = Path(os.getcwd())
+config_file = (current_dir / "job_config.json").resolve()
+custom_potential_file =  (current_dir / "custom_potential.py").resolve()
+
+DEFAULT_CONFIG: Dict[str, Any] = {
+    "method": "bounds", #"interval", "periodic"
+    "metad_mode": "wt",
+    "n_grid": 2000,
+    "sigma": 0.15,
+    "kBT": 1.0,
+    "height": 1,
+    "bias_factor": 10,
+    "potential": "constant", #"constant", "polynomial","polynomial_3d","constant_3d", "none"
+    "min": 0,
+    "max": 1,
+    "out_min": 0,
+    "out_max": 1,
+    "kernel_mode": "unit_integral",
+    "total_steps": 1_000_000_000,
+    "chunk_size":  1_000_000,
+    "stride":  10_000,
+    "deposition_pace": 500*0.001,
+    "base_dir":  "./",
+    "filename": "simulation_results.h5",
+    "hill_file": "./HILLS",
+    "filter_width": 1.5,
+    "peak_threshold": 1.0,
+    "seed": 8456835
+}
+
+def create_default_config():
+    with open(config_file, "w", encoding="utf-8") as f:
+        json.dump(DEFAULT_CONFIG, f, indent=4)
+    with open(custom_potential_file, "w") as f:
+        f.write("import numpy as np\n")
+        f.write("def custom(x, left, right):\n")
+        f.write("    pass\n")
+
+def load_config() -> SimpleNamespace:
+    with open(config_file, "r", encoding="utf-8") as f:
+        config_dict = json.load(f)
+        return SimpleNamespace(**config_dict)
+
+
---
+++ src/metad/utils.py
@@ -0,0 +1,57 @@
+import re
+import numpy as np
+from scipy.ndimage import gaussian_filter1d
+import os
+import importlib.util
+
+def natural_sort(l):
+    convert = lambda text: int(text) if text.isdigit() else text.lower()
+    alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]
+    return sorted(l, key=alphanum_key)
+
+def smoothen_log_density(p, filter_width):
+    minimum_level = max(np.max(p) * 1.0e-300, np.finfo(float).tiny)
+    logp = np.log(np.maximum(p, minimum_level))
+    smooth_logp = gaussian_filter1d(
+        logp,
+        sigma=filter_width,
+        mode="nearest",
+    )
+    return smooth_logp
+
+def make_grid(left, right, n_grid):
+    """Midpoint cells on [left, right]."""
+    edges = np.linspace(left, right, n_grid + 1)
+    dx = (right - left) / n_grid
+    x = 0.5 * (edges[:-1] + edges[1:])
+    return x, edges, dx
+
+
+def make_nested_grid(out_left, out_right, left, right, n_grid):
+    dx = (right - left) / n_grid
+
+    n_left = max(0, int(np.ceil((left - out_left) / dx)))
+    n_right = max(0, int(np.ceil((out_right - right) / dx)))
+
+    adjusted_out_left = left - n_left * dx
+    adjusted_out_right = right + n_right * dx
+
+    total_cells = n_left + n_grid + n_right
+
+    edges = np.linspace(adjusted_out_left, adjusted_out_right, total_cells + 1)
+    x = 0.5 * (edges[:-1] + edges[1:])
+
+    return x, edges, dx
+
+
+
+def import_custom_potential(module_name="custom_potential", func_name="custom"):
+    cwd = os.getcwd()
+    file_path = os.path.join(cwd, f"{module_name}.py")
+
+    spec = importlib.util.spec_from_file_location(module_name, file_path)
+    user_module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(user_module)
+
+    return getattr(user_module, func_name)
+
