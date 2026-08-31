6df04e01692e966c990dd2c87fa81ec6da406466
Olga Rogacheva <acerlaetum@yandex.ru>
2026-09-01 00:12:49 +0200

    Update config_manager.py

--- src/eqmetad/config_manager.py
+++ src/eqmetad/config_manager.py
@@ -40,7 +40,7 @@ def create_default_config():
     with open(custom_potential_file, "w") as f:
         f.write("import numpy as np\n")
         f.write("def custom(x, left, right):\n")
-        f.write("    pass\n")
+        f.write("    return np.zeros_like(x)\n")
 
 def load_config() -> SimpleNamespace:
     with open(config_file, "r", encoding="utf-8") as f:
