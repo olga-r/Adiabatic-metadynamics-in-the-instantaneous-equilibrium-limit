f019fa4e92c960dd55bc5b229d0b2a03e6b079d4
Olga Rogacheva <acerlaetum@yandex.ru>
2026-09-14 01:32:52 +0200

    Update gentraj.py

--- src/eqmetad/gentraj.py
+++ src/eqmetad/gentraj.py
@@ -223,8 +223,6 @@ def run_chunk(
         bias_centered,
         bias_level,
         time,
-        center,
-        height_step,
         history_steps[:save_idx],
         history_center[:save_idx],
         history_height[:save_idx],
