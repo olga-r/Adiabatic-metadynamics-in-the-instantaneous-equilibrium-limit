c1c9a67cb01e0692c68df935b79f433949a4ae55
Olga Rogacheva <acerlaetum@yandex.ru>
2026-09-13 13:03:57 +0200

    Update gentraj.py

--- src/eqmetad/gentraj.py
+++ src/eqmetad/gentraj.py
@@ -203,7 +203,7 @@ def run_simulation_to_disk(
     time = 0.0
     height_step = 0.0
     center = 0.5*(left+right)
-    total_saves = total_steps // stride
+    total_saves = total_steps // stride +1
 
     with h5py.File(filename, "w") as f:
         d_steps = f.create_dataset("steps", (total_saves,), dtype="i8")
