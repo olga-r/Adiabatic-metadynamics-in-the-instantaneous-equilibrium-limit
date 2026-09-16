271e1871b96d6e35e0f5a3485cbe519c6708db42
Olga Rogacheva <acerlaetum@yandex.ru>
2026-09-16 11:42:36 +0200

    Update analyze_target_distribution.py

--- src/eqmetad/analyze_target_distribution.py
+++ src/eqmetad/analyze_target_distribution.py
@@ -99,6 +99,9 @@ def main() -> None:
             json.dump(all_data, ff, indent=4)
         np.savetxt('mass_in_peaks.txt', mass_in_peaks)
         np.savetxt('KL_distance.txt', kl_distance)
+        if cfg.debug:
+            np.savetxt('heights.txt', heights)
+            np.savetxt('centers.txt', hills_centers)
         np.savetxt('time.txt', time)
         np.save("pw", f['cell_mass_pw'][:]/dx)
         np.save("pb", f['cell_mass_pb'][:]/dx)
